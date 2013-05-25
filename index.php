ebed70e89d2c884633aef3b2826a1faddf99c55a
Alina24 <abichok.@mail.ru>
2013-05-25 12:33:10 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<head>
+<title> Window_calc </title>
+</head>
+<body>
+<a href="calc.php"> Посчитать</a><br>
+ <a href="1.php"> Заказать</a>
+</body>
+</html>
